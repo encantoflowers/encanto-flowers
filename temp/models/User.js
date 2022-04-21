a42c39676b36edb3e44f1bc4aaf0cee671fbc1e4
CodeNinja <inicholas8686@gmail.com>
2022-04-21 11:07:48 -0700

    added some dependencies and starting the routes for User

--- temp/models/User.js
+++ temp/models/User.js
@@ -1,1 +1,33 @@
-// just to fill folder with something
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    maxlength: 32,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+    maxlength: 32,
+  },
+  hashed_password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  history: {
+    type: Array,
+    default: [],
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
