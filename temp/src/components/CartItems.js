2ff553bf257490627e4cfbe7b9db4f9016ef20ea
Slater McArdle <slater.mcardle@outlook.com>
2022-05-04 21:36:30 -0700

    merge commit.

---
+++ temp/src/components/CartItems.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { QuantityPicker } from 'react-qty-picker';
+
+function CartItems(item) {
+    const [subtotal, setSubtotal] = useState(item.price* item.quantity);
+
+    return (
+        <div className="cart-item" key={item.id}>
+            <img src={item.imageLink}/>
+            <div>{item.name}</div>
+            <div>{item.price}</div>
+            <QuantityPicker value={item.quantity} onChange={(value) => {
+                setSubtotal(item.price * value);
+            }} />
+            <div>{subtotal}</div>
+        </div>
+    )
+}
+
+export default CartItems;
