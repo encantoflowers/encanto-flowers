ab6a77534770c83cce5571dd552c1197ee712863
CodeNinja <inicholas8686@gmail.com>
2022-05-12 21:23:04 -0700

    user models

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,31 @@
 import { gql } from '@apollo/client';
 
+export const ADD_USER = gql`
+    mutation addUser($user: UserInput) {
+        addUser(user: $user) {
+            name
+            email
+            password
+        }
+    }
+`;
+
+export const ADD_ORDER = gql`
+    mutation addOrder($products: [ProductInput]) {
+        addOrder(products: $products) {
+            _id
+            purchaseDate
+            products {
+                _id
+                name
+                price
+                image
+                description
+            }
+        }
+    }
+`;
+
 export const QUERY_TAGS = gql`
     query allTags {
         tags {
@@ -10,3 +36,13 @@ export const QUERY_TAGS = gql`
         }
     }
 `
+
+export const LOGIN = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            name
+            email
+            password
+        }
+    }
+`;
