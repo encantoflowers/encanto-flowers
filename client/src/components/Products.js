12a3b949e1a389756aa1687dd9e016afdd9f653d
Joanna Christian <joanna.s.christian@gmail.com>
2022-05-15 23:13:05 -0700

    created component for all products

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,22 +1,64 @@
-import React from 'react'
-import { QUERY_PRODUCTS } from '../utils/queries';
+import React, { useEffect } from 'react'
+import { QUERY_ALL_PRODUCTS } from '../utils/queries';
+import {
+    UPDATE_PRODUCTS
+} from '../utils/actions';
+import { useQuery } from '@apollo/client';
+import { idbPromise } from '../../utils/helpers';
+import { useStoreContext } from '../../utils/GlobalState';
+import ProductItem from '../ProductItem';
 
 export default function AllProducts() {
 
-    const { loading, error, data } = useQuery(QUERY_PRODUCTS);
+    const [state, dispatch] = useStoreContext();
 
-    if (loading) return 'Loading...';
-    if (error) return `Error! ${error.message}`;
+    const { currentCategory } = state;
+
+    const { loading, data } = useQuery(QUERY_ALL_PRODUCTS);
+
+    useEffect(() => {
+        if (data) {
+            dispatch({
+                type: UPDATE_PRODUCTS,
+                products: data.products,
+            });
+            data.products.forEach((product) => {
+                idbPromise('products', 'put', product);
+            });
+        } else if (!loading) {
+            idbPromise('products', 'get').then((products) => {
+                dispatch({
+                    type: UPDATE_PRODUCTS,
+                    products: products,
+                });
+            });
+        }
+    }, [data, loading, dispatch]);
 
     function filterProducts() {
-        
+        if (!currentCategory) {
+            return state.products;
+        }
+
+        return state.products.filter(
+            (product) => product.category._id === currentCategory
+        );
     }
 
     return (
         <Container>
-        <div>AllProducts</div>
-        {/* loop through each product and generate a card */}
-
+            {/* loop through each product and generate a card */}
+            <div className="flex-row">
+                {filterProducts().map((product) => (
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={`/images/${product.image.img}`} />
+                        <Card.Body>
+                            <Card.Title>{product.name}</Card.Title>
+                            <Card.Text>{product.price}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                ))}
+            </div>
         </Container>
     )
 }
