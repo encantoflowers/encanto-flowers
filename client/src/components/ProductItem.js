62ec7d75c3d91454fd3e729973e379f26d2f6c4b
Joanna Christian <joanna.s.christian@gmail.com>
2022-05-15 21:25:28 -0700

    created product item component

--- client/src/components/ProductItem.js
+++ client/src/components/ProductItem.js
@@ -2,17 +2,74 @@ import { useQuery } from '@apollo/client';
 import React, { useEffect, useState } from 'react'
 import { QUERY_PRODUCT } from '../utils/queries';
 import { QuantityPicker } from 'react-qty-picker';
+import {
+    ADD_TO_CART,
+    UPDATE_CART_QUANTITY,
+    UPDATE_PRODUCTS
+} from '../utils/actions';
 
 
 export default function ProductItem(item) {
     const getQuantity = (value) => {
         console.log(value)
     }
+    const [state, dispatch] = useStoreContext();
+    const { id } = useParams();
+
+    const [currentProduct, setCurrentProduct] = useState({});
     const { loading, data } = useQuery(QUERY_PRODUCT);
-    const product = data?.product || [];
+    const { products, cart } = state;
+
+    useEffect(() => {
+        // already in global store
+        if (products.length) {
+          setCurrentProduct(products.find((product) => product._id === id));
+        }
+        // retrieved from server
+        else if (data) {
+          dispatch({
+            type: UPDATE_PRODUCTS,
+            products: data.products,
+          });
+    
+          data.products.forEach((product) => {
+            idbPromise('products', 'put', product);
+          });
+        }
+        // get cache from idb
+        else if (!loading) {
+          idbPromise('products', 'get').then((indexedProducts) => {
+            dispatch({
+              type: UPDATE_PRODUCTS,
+              products: indexedProducts,
+            });
+          });
+        }
+      }, [products, data, loading, dispatch, id]);
+
+    const addToCart = () => {
+        const itemInCart = cart.find((cartItem) => cartItem._id === id);
+        if (itemInCart) {
+            dispatch({
+                type: UPDATE_CART_QUANTITY,
+                _id: id,
+                purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+            });
+            idbPromise('cart', 'put', {
+                ...itemInCart,
+                purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+            });
+        } else {
+            dispatch({
+                type: ADD_TO_CART,
+                product: { ...currentProduct, purchaseQuantity: 1 },
+            });
+            idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
+        }
+    };
 
     return (
-        <Container>
+        <Container fluid >
             {/* item image
             item name
             item description
@@ -20,15 +77,21 @@ export default function ProductItem(item) {
             quantity picker
             add to cart
             call to order */}
-            <img
-                src={`/images/${item.image.img}`}
-            />
-            {item.name}
-            {item.description}
-            {item.price}
-            <QuantityPicker min={1} onChange={getQuantity} />
-            
-
+            <Row>
+                <Col>
+                    <img
+                        src={`/images/${currentProduct.image.img}`}
+                    />
+                </Col>
+                <Col>
+                    {currentProduct.name}
+                    {currentProduct.description}
+                    {currentProduct.price}
+                    <QuantityPicker min={1} onChange={getQuantity} />
+                    <Button variant="primary" onClick={addToCart}>Add to Cart</Button>
+                    <a href="tel:123456789"><Button variant="primary">Call to Order</Button></a>
+                </Col>
+            </Row>
         </Container>
     )
 }
