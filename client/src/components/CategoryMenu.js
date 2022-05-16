c16e395dcf09a4be6c0fa1643cf1ab9b6020aa89
Joanna Christian <joanna.s.christian@gmail.com>
2022-05-15 23:13:24 -0700

    created category menu component

---
+++ client/src/components/CategoryMenu.js
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { useStoreContext } from '../../utils/GlobalState';
+import {
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY,
+} from '../../utils/actions';
+import { QUERY_CATEGORIES } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
+import { Dropdown } from 'react-bootstrap'
+
+export default function CategoryMenu() {
+
+    const [state, dispatch] = useStoreContext();
+
+    const { categories } = state;
+
+    const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+    useEffect(() => {
+        if (categoryData) {
+            dispatch({
+                type: UPDATE_CATEGORIES,
+                categories: categoryData.categories,
+            });
+            categoryData.categories.forEach((category) => {
+                idbPromise('categories', 'put', category);
+            });
+        } else if (!loading) {
+            idbPromise('categories', 'get').then((categories) => {
+                dispatch({
+                    type: UPDATE_CATEGORIES,
+                    categories: categories,
+                });
+            });
+        }
+    }, [categoryData, loading, dispatch]);
+
+    const handleClick = (id) => {
+        dispatch({
+            type: UPDATE_CURRENT_CATEGORY,
+            currentCategory: id,
+        });
+    };
+    return (
+        <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Categories: All 
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                {categories.map((item) => (
+                    <Dropdown.Item key={item._id}
+                        onClick={() => {
+                            handleClick(item._id);
+                        }}>{item.name}</Dropdown.Item>
+                ))}
+            </Dropdown.Menu>
+        </Dropdown>
+    )
+}
