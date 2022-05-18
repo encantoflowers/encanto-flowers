ccf1a8c7546f045a99a9d649d88037e602fa192e
Joanna Christian <joanna.s.christian@gmail.com>
2022-05-17 22:37:14 -0700

    created Fine Print component

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import CategoryMenu from './components/CategoryMenu';
 import ProductItem from './components/ProductItem';
 import  QuantityPicker  from './components/QuantityPicker'
 import Banner from './components/Banner';
+import FinePrint from './components/FinePrint';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -45,6 +46,7 @@ function App() {
           <Banner />
         </div>
        <ProductItem />
+       <FinePrint />
         <Routes>
        
       {/* <Navbar /> */}
