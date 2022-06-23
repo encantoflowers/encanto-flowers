1190ead347c7ada712f1fdbfc91beb62ebd1f173
CodeNinja <inicholas8686@gmail.com>
2022-06-22 19:07:31 -0700

    adminCRUD

--- client/src/pages/AdminPanel.js
+++ client/src/pages/AdminPanel.js
@@ -1,10 +1,48 @@
 import React from 'react'
+import { QUERY_ALL_PRODUCTS, QUERY_CATEGORIES } from '../../utils/queries';
+import { ADD_PRODUCT, UPDATE_PRODUCT, DELETE_PRODUCT, ADD_CATEGORY, UPDATE_CATEGPRY, DELETE_CATEGORY } from '../../utils/actions';
 
 function AdminPanel() {
   return (
     <div>
-        <h1 className="p-5">You are an Admin, You Made It Here, Don't Stop Now!!!</h1>
+        <h1 className="p-5">What would you like to do today?</h1>
+    <form onSubmit={handleFormSubmit}>
+    <div className="form-group">
+      <label htmlFor="userName" className="p-2">User Name</label> 
+      <input
+      
+        type="userName"
+        name="userName"
+        id="userName"
+        placeholder="User Name"
+        onChange={handleChange}
+      />
     </div>
+    <div className="form-group mb-2">
+      <label htmlFor="email" className="p-2">Email address</label>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Email"
+        onChange={handleChange}
+      />
+    </div>
+    <div className="form-group mb-4">
+      <label htmlFor="pwd" className="p-2">Password</label>
+      <input
+        type="password"
+        name="password"
+        id="pwd"
+        placeholder="Password"
+        onChange={handleChange}
+      />
+    </div>
+    <button type="submit" className="btn btn-primary">
+      Submit
+    </button>
+  </form>
+  </div>
   )
 }
 
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const ADD_USER = gql`
   mutation addUser(
@@ -22,54 +22,139 @@ export const ADD_USER = gql`
 `;
 
 export const DELETE_USER = gql`
-  mutation deleteUser(
-    $id: ID!
-  ) {
-    deleteUser(
-      id: $id
-    ) {
-        _id
+  mutation deleteUser($id: ID!) {
+    deleteUser(id: $id) {
+      _id
     }
   }
 `;
 
 export const ADD_ORDER = gql`
-    mutation addOrder($products: [ID]!, $total: Float!) {
-        addOrder(products: $products, total: $total) {
-            _id
-            purchaseDate
-            products {
-                _id
-                name
-                price
-                description
-                image {
-                  _id
-                  description
-                  img
-                }
-            }
-            total
+  mutation addOrder($products: [ID]!, $total: Float!) {
+    addOrder(products: $products, total: $total) {
+      _id
+      purchaseDate
+      products {
+        _id
+        name
+        price
+        description
+        image {
+          _id
+          description
+          img
         }
+      }
+      total
     }
+  }
 `;
 
 export const ADD_CATEGORY = gql`
-    mutation addCategory($Name: String ) {
-        addCategory(Name: $Name) {
-            _id
-            Name
-        }
+  mutation addCategory($Name: String) {
+    addCategory(Name: $Name) {
+      _id
+      Name
     }
+  }
 `;
 
-export const REMOVE_CATEGORY = gql`
-    mutation removeCategory($Name: String! ) {
-        removeCategory(Name: $Name) {
-            _id
-            Name
-        }
+export const UPDATE_CATEGORY = gql`
+  mutation updateCategory($id: ID!, $Name: String) {
+    updateCategory(id: $id, Name: $Name) {
+      _id
+      name
     }
+  }
+`;
+
+export const DELETE_CATEGORY = gql`
+  mutation removeCategory($Name: String!) {
+    removeCategory(Name: $Name) {
+      _id
+      Name
+    }
+  }
+`;
+
+export const ADD_PRODUCT = gql`
+  mutation addProduct(
+    $name: String!
+    $description: String!
+    $price: Float!
+    $image: String!
+    $categories: [ID]!
+  ) {
+    addProduct(
+      name: $name
+      description: $description
+      price: $price
+      image: $image
+      categories: $categories
+    ) {
+      _id
+      name
+      description
+      price
+      image {
+        _id
+        description
+        img
+      }
+      categories {
+        _id
+        Name
+      }
+    }
+  }
+`;
+
+export const UPDATE_PRODUCT = gql`
+  mutation updateProduct(
+    $_id: ID!
+    $name: String!
+    $description: String!
+    $price: Float!
+    $image: String!
+    $categories: [ID]!
+    $featured: Boolean
+    $seasonal: Boolean
+  ) {
+    updateProduct(
+      _id: $_id
+      name: $name
+      description: $description
+      price: $price
+      image: $image
+      categories: $categories
+      featured: $featured
+      seasonal: $seasonal
+    ) {
+      _id
+      name
+      description
+      price
+      image {
+        _id
+        description
+        img
+      }
+      categories {
+        _id
+        Name
+      }
+      featured 
+      seasonal
+    }
+  }
+`;
+
+export const DELETE_PRODUCT = gql`
+  mutation removeProduct($_id: ID!) {
+    removeProduct(_id: $_id) {
+      _id
+    }
+  }
 `;
 
 export const LOGIN = gql`
@@ -82,4 +167,3 @@ export const LOGIN = gql`
     }
   }
 `;
-
