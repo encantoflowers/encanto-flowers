b00dcd11b7336e4a6a9efb83b809a60d95098bf1
CodeNinja <inicholas8686@gmail.com>
2022-06-23 16:19:49 -0700

    adminCRUDTest

--- client/src/pages/AdminPanel.js
+++ client/src/pages/AdminPanel.js
@@ -1,49 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { QUERY_ALL_PRODUCTS, QUERY_CATEGORIES } from '../../utils/queries';
 import { ADD_PRODUCT, UPDATE_PRODUCT, DELETE_PRODUCT, ADD_CATEGORY, UPDATE_CATEGPRY, DELETE_CATEGORY } from '../../utils/actions';
+import { useStoreContext } from '../../utils/GlobalState';
+import {
+    UPDATE_CURRENT_CATEGORY,
+} from '../../utils/actions';
+import { useParams } from 'react-router-dom';
+import { Dropdown, DropdownButton, ButtonGroup, Container } from 'react-bootstrap'
+import './style.css'
 
-function AdminPanel() {
+export default function CategoryMenu() {
+
+  const [state, dispatch] = useStoreContext();
+
+  const { category } = useParams();
+  const decodedCategory = category.replace("%20", " ")
+
+  const { categories } = state;
+
+  const [categoryName, updateCategoryName] = useState('All');
+
+  useEffect(() => {
+          updateCategoryName(decodedCategory);
+  }, [])
+
+  const handleClick = (id) => {
+      dispatch({
+          type: UPDATE_CURRENT_CATEGORY,
+          currentCategory: id,
+      });
+      updateCategoryName((categories.find((category)=> category._id === id)).Name);
+  };
+
   return (
-    <div>
-        <h1 className="p-5">What would you like to do today?</h1>
-    <form onSubmit={handleFormSubmit}>
-    <div className="form-group">
-      <label htmlFor="userName" className="p-2">User Name</label> 
-      <input
-      
-        type="userName"
-        name="userName"
-        id="userName"
-        placeholder="User Name"
-        onChange={handleChange}
-      />
-    </div>
-    <div className="form-group mb-2">
-      <label htmlFor="email" className="p-2">Email address</label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        placeholder="Email"
-        onChange={handleChange}
-      />
-    </div>
-    <div className="form-group mb-4">
-      <label htmlFor="pwd" className="p-2">Password</label>
-      <input
-        type="password"
-        name="password"
-        id="pwd"
-        placeholder="Password"
-        onChange={handleChange}
-      />
-    </div>
-    <button type="submit" className="btn btn-primary">
-      Submit
-    </button>
-  </form>
-  </div>
+<Container className='buttongroup-container'>
+      <DropdownButton as={ButtonGroup} title={`Category: ${categoryName}`} className='category-dropdown mx-auto mt-4' variant="light" >
+              {categories ? (
+                  <div>
+                      {categories.map((category) => (
+                          <Dropdown.Item key={category._id}
+                              onClick={(e) => {
+                                  e.preventDefault()
+                                  handleClick(category._id)
+                              }}>{category.Name}
+                          </Dropdown.Item>
+                      ))}
+                  </div>
+              ) : (
+                  <div>
+                      <Dropdown.Item href="#" key='None'>'Categories loading...'</Dropdown.Item>
+                  </div>
+              )}
+      </DropdownButton>
+      </Container>
   )
 }
-
-export default AdminPanel;
